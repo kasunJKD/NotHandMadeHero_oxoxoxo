432d448a6138c0218cc251c159f8a19d300bcc8f
kasunJKD <kasunjayakodyofficial@gmail.com>
2023-02-15 22:58:42 +0530

    build script changed

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir ..\build
-pushd ..\build
-cl /Zi ..\code\win32_scratch.cpp user32.lib
+mkdir ..\..\build
+pushd ..\..\build
+cl /Zi ..\NotHandMadeHero_oxoxoxo\code\win32_scratch.cpp user32.lib
 popd
