ea0b5c1b8e4100fba47dabf9ab38d1e6bf4ded2c
kasunJKD <kasunjayakodyofficial@gmail.com>
2023-04-04 21:10:21 +0530

    day 4

--- code/build.bat
+++ code/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl /Zi ..\NotHandMadeHero_oxoxoxo\code\win32_scratch.cpp user32.lib Gdi32.lib
+cl -FC -Zi ..\NotHandMadeHero_oxoxoxo\code\win32_scratch.cpp user32.lib Gdi32.lib
 popd
